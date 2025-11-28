eeaec4962b2f88f62ffaa6e7c20be5f789be983a
chbadad@cloud-1.42 <you@example.com>
2025-11-28 12:45:23 +0100

    correction nasm, changement de nom entre ALIGN et MBALIGN..

--- srcs/boot.asm
+++ srcs/boot.asm
@@ -40,10 +40,10 @@ stack_top:
 section .text
 global _start:function (_start.end - _start)
 _start:
-	mov esp, stack_top
-	call kernel_main
-	cli
-.halt:  hlt
-	jmp .halt
+	mov esp, stack_top; creation de la stack
+	call kernel_main; call du point d'entree du kernel
+	cli; empeche les interruptions de survenir jusqu'a ce que l'instruction STI soit rencontree
+.halt:  hlt; mais le kernel dans un etat en suspension
+	jmp .halt; loop infinie dans l'etat en suspension
 .end:
 
